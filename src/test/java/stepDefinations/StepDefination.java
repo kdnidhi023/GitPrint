96186ce3e66be3363ccf0776af49dd8195ef759d
Anita <kdnidhi023@gmail.com>
2020-09-27 01:01:20 +0530

    ByNidhi

--- src/test/java/stepDefinations/StepDefination.java
+++ src/test/java/stepDefinations/StepDefination.java
@@ -7,5 +7,6 @@ public class StepDefination {
     @Given("^statement to print$")
     public void statement_to_print() throws Throwable {
         System.out.println("This is my first command");
+        System.out.println("This is my second command added by Nidhi");
 }
 }
